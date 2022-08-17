fdfdc9a94595ddccc438fe277f5d45bb9c1da1b5
Eric Bower <me@erock.io>
2022-08-17 12:48:26 -0400

    make default the pentest for now

--- cmd/root.go
+++ cmd/root.go
@@ -59,7 +59,7 @@ func NewRootCmd() *cobra.Command {
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "common", "", "common file (default is $HOME/.aptible.yaml)")
 	rootCmd.PersistentFlags().StringVar(&token, "token", "", "jwt token")
-	rootCmd.PersistentFlags().StringVar(&authDomain, "auth-domain", "auth-api-master.aptible-staging.com", "auth domain")
+	rootCmd.PersistentFlags().StringVar(&authDomain, "auth-domain", "auth-pentest.aptible-sandbox.com", "auth domain")
 	rootCmd.PersistentFlags().StringVar(&apiDomain, "api-domain", "cloud-api.aptible.com", "api domain")
 	rootCmd.PersistentFlags().StringVar(&org, "org", "", "organization id")
 	rootCmd.PersistentFlags().StringVar(&env, "env", "", "environment id")
